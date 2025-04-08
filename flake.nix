abe16cb697c7998b5c2a28b23f58455f5a272020
Finn Milner <finn.milner@outlook.com>
2025-04-08 08:21:53 +0100

    refactor: remove non existent override

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
       url = "github:nix-community/home-manager";
